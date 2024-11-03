a0b258bae514ed99fe8102294cf1b02e7a298e9f
Abhilash_Titus <abilashtitus@gmail.com>
2024-11-03 13:49:47 +0530

    Navbar Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
